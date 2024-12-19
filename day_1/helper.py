918a16bc33fbf4545e6a6f0512380d7e125291d8
saadberry <saadberry29@gmail.com>
2024-12-19 11:48:25 +0500

    day1/proces_input_data

---
+++ day_1/helper.py
@@ -0,0 +1,40 @@
+"""
+File containing input for day 1
+"""
+
+class Helper:
+    """
+    Helper class for the question
+    """
+
+    def __init__(self):
+        self.left = []
+        self.right = []
+
+    def process_input(self):
+        """
+        Method that process input file
+        """
+        file_path = "input.txt"
+        # Read the file
+        with open(file_path, "r") as file:
+            for line in file:
+                if line.strip():
+                    left, right = map(int, line.split())
+                    self.left.append(left)
+                    self.right.append(right)
+
+    def get_lists(self):
+        """
+        Method that returns processed input data
+        Returns:
+            - <dict>: Dict of left and right lists
+        """
+        return {'left':self.left, 'right': self.right}
+
+
+if __name__ == "__main__":
+    helper = Helper()
+    result = helper.process_input()
+    output = helper.get_lists()
+    print(f"result={output}")
