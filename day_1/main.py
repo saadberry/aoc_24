ed38c225fa9a9d1e4b1da5d236553963239e232a
saadberry <saadberry29@gmail.com>
2024-12-26 11:26:18 +0500

    Part 2: Computing similarity score

--- day_1/main.py
+++ day_1/main.py
@@ -3,7 +3,7 @@
 """
 from helper import Helper 
 
-class ReconcileLists:
+class Day1:
     """
     Class that reconciles lists for the historians 
     """
@@ -25,6 +25,29 @@ class ReconcileLists:
             sum += abs(left[i] - right[i])
             i += 1
         return sum
+    
+    def ComputeSimilarityScore(self):
+        """
+        Class that computes similarity scores
+        Flow:
+            - Fill a hash map of second list as: {'element': frequency}
+            - Iterate through the first list, multiply the elements by their key in the hash map
+            - Add that value to similarity_score
+            - Return similarity_score
+        """
+        lists = Helper().get_lists()
+        left, right = lists["left"], lists["right"]
 
-if __name__ == "__main__":
-    print(f"total distance = {ReconcileLists().reconcile()}")
+        similarity_score = 0
+        # Populating hash map
+        h_map = {}
+        for element in range(len(right)):
+            if right[element] not in h_map:
+                h_map[right[element]] = 1
+            else:
+                h_map[right[element]] += 1
+        # Iterating first list
+        for element in range(len(left)):
+            # Add result of product of element with their key in the hash map
+            similarity_score += left[element] * (h_map[left[element]] if left[element] in h_map else 0)
+        return similarity_score
